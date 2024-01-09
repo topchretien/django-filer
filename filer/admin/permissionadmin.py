b988af3bfdcd4e01b9f8f8d5b9899cb63075b4bd
florent.veillon <florent.veillon@nyleza.com>
2024-01-09 17:27:43 +0100

    remove unused import

--- filer/admin/permissionadmin.py
+++ filer/admin/permissionadmin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 
 from .. import settings
-from ..fields import folder
 
 
 class PermissionAdmin(admin.ModelAdmin):
